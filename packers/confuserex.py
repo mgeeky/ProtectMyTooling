e722a76444b603d87958860e344973594a300f3b
Mariusz B. / mgeeky <mb@binary-offensive.com>
2022-04-29 01:56:27 +0200

    Fix in ConfuserEx

--- packers/confuserex.py
+++ packers/confuserex.py
@@ -7,6 +7,9 @@ from lib.utils import *
 import xml.etree.ElementTree as ET
 import shutil
 
+from pathlib import Path
+
+
 class PackerConfuserEx(IPacker):
     default_confuserex_args = '-n'
     confuserex_cmdline_template = '<command> <options> <infile>'
@@ -19,6 +22,7 @@ class PackerConfuserEx(IPacker):
         self.confuserex_args = PackerConfuserEx.default_confuserex_args
         self.logger = logger
         self.options = options
+        self.modules = []
 
     @staticmethod
     def get_name():
@@ -42,6 +46,12 @@ class PackerConfuserEx(IPacker):
             parser.add_argument('--confuserex-args', metavar='ARGS', dest='confuserex_args',
                 help = 'Optional ConfuserEx-specific arguments to pass during compression.')
 
+            parser.add_argument('--confuserex-module', metavar='PATH', dest='confuserex_module', nargs='+', action='extend',
+                help = '(Optional) Embed specified by path DLL module into final EXE. Can be repeated.')
+
+            parser.add_argument('--confuserex-modules-in-dir', metavar='DIR', dest='confuserex_modules_in_dir', nargs='+', action='extend',
+                help = '(Optional) Embed all DLLs in specified DIR into final EXE. Can be repeated.')
+
         else:
             if not self.options['config']:
                 self.logger.fatal('Config file not specified!')
@@ -55,6 +65,19 @@ class PackerConfuserEx(IPacker):
             for k, v in PackerConfuserEx.default_options.items():
                 if k not in self.options.keys():
                     self.options[k] = v
+
+            if 'confuserex_module' in self.options.keys() and self.options['confuserex_module'] != None \
+                and len(self.options['confuserex_module']) > 0:
+                for mod in self.options['confuserex_module']:
+                    self.logger.dbg(f'Will embed DLL into EXE: {mod}')
+                    self.modules.append(mod)
+
+            if 'confuserex_modules_in_dir' in self.options.keys() and self.options['confuserex_modules_in_dir'] != None \
+                and len(self.options['confuserex_modules_in_dir']) > 0:
+                for d in self.options['confuserex_modules_in_dir']:
+                    for path in Path(d).rglob('*.dll'):
+                        self.logger.dbg(f'Will embed DLL into EXE: {path.name}')
+                        self.modules.append(path.name)
 
             if 'confuserex_args' in self.options.keys() and self.options['confuserex_args'] != None \
                 and len(self.options['confuserex_args']) > 0: 
@@ -83,13 +106,19 @@ class PackerConfuserEx(IPacker):
         et.attrib['outputDir'] = outputDir
         et.attrib['baseDir'] = os.path.dirname(infile)
 
-        ET.SubElement(et, 'module').attrib['path'] = os.path.basename(infile)
-        ET.SubElement(et, 'probePath').text = os.path.dirname(infile)
-
         dirA = os.path.normpath(os.path.join(os.path.dirname(infile), '..'))
         dirB = os.path.normpath(os.path.join(os.path.dirname(infile), '../..'))
         dirC = os.path.normpath(os.path.join(os.path.dirname(infile), '../../..'))
 
+        if len(self.modules) > 0:
+            ET.SubElement(et, 'packer').attrib['id'] = 'compressor'
+
+        ET.SubElement(et, 'module').attrib['path'] = os.path.basename(infile)
+        
+        for mod in self.modules:
+            ET.SubElement(et, 'module').attrib['path'] = mod
+        
+        ET.SubElement(et, 'probePath').text = os.path.dirname(infile)
         if os.path.isdir(dirA): ET.SubElement(et, 'probePath').text = dirA
         if os.path.isdir(dirB): ET.SubElement(et, 'probePath').text = dirB
         if os.path.isdir(dirC): ET.SubElement(et, 'probePath').text = dirC
@@ -124,6 +153,12 @@ Adjusted project file:
 
             tmpname = ''
             try:
+                cwd = os.getcwd()
+                base = os.path.dirname(infile)
+
+                self.logger.dbg('changed working directory to "{}"'.format(base))
+                os.chdir(base)
+
                 with tempfile.NamedTemporaryFile(delete=False, suffix='.crproj') as fp: 
                     self.adjustProjectFile(fp, arch, infile, outfile, tmpdir)
                     tmpname = fp.name
@@ -174,6 +209,9 @@ Adjusted project file:
                 raise
 
             finally:
+                self.logger.dbg('reverted to original working directory "{}"'.format(cwd))
+                os.chdir(cwd)
+
                 if os.path.isfile(tmpname): 
                     os.remove(tmpname)
 
