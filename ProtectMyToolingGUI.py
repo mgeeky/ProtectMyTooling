c3f658e33199a7818224a7e5918ff6d51db671c4
Mariusz B. / mgeeky <mb@binary-offensive.com>
2022-07-09 12:10:20 +0200

    Improved GUI

--- ProtectMyToolingGUI.py
+++ ProtectMyToolingGUI.py
@@ -9,61 +9,89 @@
 
 import time
 import os, sys
+import yaml
+import random
 
 from lib.utils import *
 from lib.logger import Logger
 from lib.packersloader import PackersLoader
+from ProtectMyTooling import VERSION
 import lib.optionsparser
 
 import PySimpleGUI as sg
 
+sg.theme("Dark")
+font = ("Consolas", 8)
+font2 = ("Consolas", 10)
 
-font = ("Consolas", 9)
-font2 = ("Consolas", 11)
+phrases = (
+    'with great power, comes great responsibility.',
+    "don't detect tools, detect techniques",
+    'to be used in ethical offensive assessments only!',
+    'support hard-working open-source Offensive Security Tools developers :)',
+    'be responsible - watermark and track your implants',
+    'be responsible - collect your implants\'s IOCs'
+)
 
 # https://stackoverflow.com/a/69064884
 def runCommand(cmd, timeout=None, window=None):
+    cwd = os.getcwd()
+
+    try:
+        p = cmd[0]
+        if len(cmd) > 1:
+            p = cmd[1]
+
+        d = os.path.dirname(os.path.normpath(os.path.abspath(p)))
+        if os.path.isdir(d):
+            os.chdir(d)
+
+        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = ''
+
+        for line in p.stdout:
+            line = line.decode(errors='replace' if (sys.version_info) < (3, 5) else 'ignore').rstrip()
+            output += line
+            print(line)
+
+            if window:
+                window.Refresh()
+                window['-outputml-'].set_vscroll_position(1.0)
+            
+        retval = p.wait(timeout)
+        return (retval, output)  
     
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output = ''
-
-    for line in p.stdout:
-        line = line.decode(errors='replace' if (sys.version_info) < (3, 5) else 'ignore').rstrip()
-        output += line
-        print(line)
-        window.Refresh() if window else None
-
-    retval = p.wait(timeout)
-    return (retval, output)                         
+    finally:
+        os.chdir(cwd)
 
 def run(command, width = 120):
-    sg.theme("Dark")
-
     layout = [
         [
             sg.Text("ProtectMyTooling output:", font=font),
         ],
         [
-            sg.Multiline(size=(width, 60), font=font, echo_stdout_stderr=False, reroute_stdout=True, reroute_stderr=True, no_scrollbar=False, horizontal_scroll=True)
+            sg.Multiline(size=(width, 40), key='-outputml-', font=font, echo_stdout_stderr=False, reroute_stdout=True, reroute_stderr=True, no_scrollbar=False, horizontal_scroll=True)
         ],
         [
             sg.Button("Close", key="-Exit-", font=font),
         ],
     ]
 
-    window = sg.Window("Protection in progres...", layout, modal=True, finalize=True)
+    window = sg.Window("Protection in progres...", 
+                        layout, modal=True, finalize=True, resizable=True, return_keyboard_events=True)
+    window.bind("<Escape>", "-Exit-")
+
+    print(f'PS> {" ".join(command)}\n')
     runCommand(cmd=command, window=window)
   
     while True:
         event, values = window.read()
-        if event == "-Exit-" or event == sg.WIN_CLOSED:
+        if event == "-Exit-" or event == sg.WIN_CLOSED or event == 'Escape:27':
             break
 
     window.close()
 
 def editConfig(config):
-    sg.theme("Dark")
-
     layout = [
         [
             sg.Text(config, font=font),
@@ -77,14 +105,15 @@ def editConfig(config):
         ],
     ]
 
-    window = sg.Window("Protection in progres...", layout, modal=True, finalize=True)
+    window = sg.Window("Protection in progres...", layout, modal=True, finalize=True, return_keyboard_events=True)
+    window.bind("<Escape>", "-Exit-")
     
     with open(config) as f:
         print(f.read())
   
     while True:
         event, values = window.read()
-        if event == "-Exit-" or event == sg.WIN_CLOSED:
+        if event == "-Exit-" or event == sg.WIN_CLOSED or event == 'Escape:27':
             break
 
         if event == "-Save-":
@@ -97,25 +126,25 @@ def editConfig(config):
     window.close()
 
 def createWindow(packersChain, packersList):
-    sg.theme("Dark")
-
-    tooltip1 = 'Inject watermark to generated artifact. Syntax: where=value, example: "-w dos-stub=Foobar". Available watermark places: dos-stub,checksum,overlay,section . Section requires NAME,STR syntax where NAME denotes PE section name, e.g. "-w section=.foo,bar" will create PE section named ".foo" with contents "bar". May be repeated'
+    tooltip1 = 'Inject watermark to generated artifact. Syntax: where=value, e.g.: "dos-stub=Foobar".\nAvailable watermarks: dos-stub,checksum,overlay,section\nSection requires NAME,STR syntax where NAME denotes PE section name\ne.g. "section=.foo,bar" creates PE section named ".foo" with contents "bar".\nMay be repeated'
     tooltip2 = 'Specify a custom IOC value that is to be written into output IOCs csv file in column "comment"'
 
     params_column = [
         [
-            sg.Text("Input File" + ' ' * 7, font=font ),
-            sg.Input(size=(50, 1), enable_events=True, key="-infile-", font=font),
+            sg.Text("Input File" + ' ' * 1, font=font, tooltip='Input implant file to be obfuscated/protected' ),
+            sg.Input(size=(60, 1), enable_events=True, key="-infile-", font=font, tooltip='Input implant file to be obfuscated/protected' ),
             sg.FileBrowse(font=font),
         ],
         [
-            sg.Text("Output Folder" + ' ' * 4, font=font),
-            sg.Input(size=(50, 1), enable_events=True, key="-outdir-", font=font),
-            sg.FolderBrowse(font=font),
+            sg.Text("Output File", font=font, tooltip='Output obfuscated/protected file.'),
+            sg.Input(size=(60, 1), enable_events=True, key="-outfile-", font=font, tooltip='Output obfuscated/protected file.'),
+            sg.FileSaveAs(font=font),
         ],
         [
-            sg.Text("Output File name" + ' ' * 1, font=font),
-            sg.Input(size=(50, 1), enable_events=True, key="-outfilename-", font=font),
+            sg.Text("File Architecture" + ' ' * 1 , font=font, tooltip='Specify input file architecture, or leave "Auto" to make script auto detect it.'),
+            sg.Combo(size=(10, 1), values=["Auto", "x86", "x64"], readonly=True, default_value="Auto", enable_events=True, key="-arch-", font=font),
+            sg.Text("Detected file type: " , font=font),
+            sg.Text("" , font=font, key='-detected-', text_color='cyan'),
         ],
         [
             sg.Column([
@@ -124,15 +153,6 @@ def createWindow(packersChain, packersList):
                 ]
             ]),
             sg.Column([
-                [sg.Text("", font=font2)],
-                [
-                    sg.Text("File Architecture" , font=font),
-                    sg.Combo(size=(10, 1), values=["Auto", "x86", "x64"], readonly=True, default_value="Auto", enable_events=True, key="-arch-", font=font),
-                ],
-                [
-                    sg.Text("Detected file type: " , font=font),
-                    sg.Text("" , font=font, key='-detected-'),
-                ],
                 [sg.Text("", font=font2)],
                 [sg.Text("<-- Packers chain", font=font2)],
                 [sg.Text("", font=font2)],
@@ -145,22 +165,22 @@ def createWindow(packersChain, packersList):
         ],
         [
             sg.Text("Config path", font=font),
-            sg.Input(size=(56, 1), default_text=os.path.abspath(os.path.join(os.path.dirname(__file__), "config/ProtectMyTooling.yaml")), enable_events=True, key="-config-", font=font),
+            sg.Input(size=(65, 1), default_text=os.path.abspath(os.path.join(os.path.dirname(__file__), "config/ProtectMyTooling.yaml")), enable_events=True, key="-config-", font=font),
             sg.FileBrowse(font=font),
         ],
         [
             sg.Text("Watermark" + ' ' * 2, font=font, tooltip=tooltip1),
-            sg.Input(size=(65, 1), default_text="section=.foo,1234567890abcdef123456", pad=(5,5), enable_events=True, key="-watermark-", font=font, tooltip=tooltip1),
+            sg.Input(size=(74, 1), default_text="section=.foo,1234567890abcdef123456", pad=(5,5), enable_events=True, key="-watermark-", font=font, tooltip=tooltip1),
         ],
         [
             sg.Text("Custom IOC" + ' ' * 1, font=font, tooltip=tooltip2),
-            sg.Input(size=(65, 1), default_text="", pad=(5,5), enable_events=True, key="-customioc-", font=font, tooltip=tooltip2),
+            sg.Input(size=(74, 1), default_text="", pad=(5,5), enable_events=True, key="-customioc-", font=font, tooltip=tooltip2),
         ],
         [
-            sg.Checkbox('Collect IOCs', default=False, tooltip="Collect IOCs and save them to .csv file side by side to <outfile>", font=font),
-            sg.Checkbox('Hide Console', default=False, tooltip="If output artifact is PE EXE, use this option to hide Console window by switching PE Subsystem from WINDOWS_GUI", font=font),
-            sg.Checkbox('Verbose', default=True, font=font),
-            sg.Checkbox('Debug', default=False, font=font)
+            sg.Checkbox('Collect IOCs', key='-Collect IOCs-', text_color='cyan', default=False, tooltip="Collect IOCs and save them to .csv file side by side to <outfile>", font=font),
+            sg.Checkbox('Hide Console', key='-Hide Console-', default=False, tooltip="If output artifact is PE EXE, use this option to hide Console window by switching PE Subsystem from WINDOWS_GUI", font=font),
+            sg.Checkbox('Verbose', key='-Verbose-', default=True, font=font),
+            sg.Checkbox('Debug' , key='-Debug-', default=False, font=font)
         ]
     ]
 
@@ -200,8 +220,11 @@ def createWindow(packersChain, packersList):
         ]
     ]
 
-    window = sg.Window("ProtectMyTooling | with great power, comes great responsibility. ", 
-                        layout, return_keyboard_events=True, resizable=True)
+    phrase = random.choice(phrases)
+    window = sg.Window(f"ProtectMyTooling v{VERSION} | {phrase} ", 
+                        layout, return_keyboard_events=True, resizable=True, finalize=True)
+    window.bind("<Escape>", "-Exit-")
+
     return window
 
 def main():
@@ -291,7 +314,7 @@ def main():
                 chain = f'"{os.path.basename(values["-infile-"])}"'
                 for p in packersChain:
                     chain = f"{p[0].capitalize()}({chain})"
-                window['-current chain-'].update(chain) 
+                window['-current chain-'].update(chain)
             
         elif event == "About":
             sg.Popup("About ProtectMyTooling", f'''
@@ -319,11 +342,48 @@ Enjoy!
             p = os.path.normpath(os.path.abspath(values["-infile-"]))
             path, ext = os.path.splitext(p)
             newname = os.path.basename(path) + '-obf' + ext
-            window["-outfilename-"].update(os.path.basename(newname))
             window["-infile-"].update(p)
 
-            if len(values["-outdir-"]) == 0:
-                window["-outdir-"].update(os.path.dirname(p))
+            if len(values["-outfile-"]) == 0:
+                outpath = os.path.normpath(os.path.abspath(os.path.join(os.path.dirname(p), newname)))
+                window["-outfile-"].update(outpath)
+
+            ftype = 'unkown'
+
+            if isDotNetExecutable(p): 
+                window['-detected-'].update('.NET Assembly')
+                ftype = 'dotnet'
+            elif isValidPE(p): 
+                window['-detected-'].update('PE Executable')
+                ftype = 'pe'
+            elif isShellcode(p): 
+                window['-detected-'].update('Shellcode')
+                ftype = 'shellcode'
+            else: 
+                window['-detected-'].update('Unknown')
+
+            if len(values['-config-']) > 0 and os.path.isfile(values['-config-']):
+                try:
+                    with open(values['-config-']) as f:
+                        parsed = yaml.load(f, Loader=yaml.FullLoader)
+
+                    if ftype == 'pe': 
+                        packersChain = [[x] for x in parsed['gui_default_chain_pe'].replace(' ', '').split(',')]
+                    if ftype == 'dotnet': 
+                        packersChain = [[x] for x in parsed['gui_default_chain_dotnet'].replace(' ', '').split(',')]
+                    if ftype == 'shellcode': 
+                        packersChain = [[x] for x in parsed['gui_default_chain_shellcode'].replace(' ', '').split(',')]
+
+                    index = 0
+                    window["-packers chain-"].update(packersChain, set_to_index=[index + 1], scroll_to_index=index + 1)
+
+                    chain = f'"{os.path.basename(values["-infile-"])}"'
+                    for p in packersChain:
+                        chain = f"{p[0].capitalize()}({chain})"
+                    window['-current chain-'].update(chain)
+
+                except:
+                    pass
 
         elif event == "Edit Config":
             if len(values["-config-"]) > 0:
@@ -355,7 +415,7 @@ Enjoy!
             watermark = values["-watermark-"]
             config = values["-config-"]
             customioc = values["-customioc-"]
-            outfile = os.path.normpath(os.path.abspath(os.path.join(values["-outdir-"], values["-outfilename-"])))
+            outfile = os.path.normpath(os.path.abspath(values["-outfile-"]))
             packers = ','.join([x[0] for x in packersChain])
 
             with tempfile.NamedTemporaryFile() as temp:
@@ -372,26 +432,25 @@ Enjoy!
                     '-l',
                     logpath,
                     '-C',
-                    '--arch',
-                    arch,
                     infile,
                     outfile
                 ]
 
+                if arch.lower() != 'auto': command.extend(['--arch', arch, ])
                 if len(watermark) > 0: command.extend(['-w', watermark])
                 if len(customioc) > 0: command.extend(['-I', customioc])
 
-                if values['Collect IOCs']: command.append('-i')
-                if values['Hide Console']: command.append('-g')
-                if values['Verbose']: command.append('-v')
-                if values['Debug']: command.append('-d')
+                if values['-Collect IOCs-']: command.append('-i')
+                if values['-Hide Console-']: command.append('-g')
+                if values['-Verbose-']: command.append('-v')
+                if values['-Debug-']: command.append('-d')
 
                 if event == "Protect & Run": command.append('-r')
 
                 if os.path.isfile(outfile):
                     os.remove(outfile)
 
-                run(command, logpath)
+                run(command)
 
     window.close()
 
